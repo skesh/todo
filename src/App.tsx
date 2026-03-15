a8c79801f69cbb52ac358047441240c1793fbabd
skesh <mail@skesh.ru>
2026-03-15 21:22:09 +0300

    feat: add todo mechanism

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 import { ITodo } from './interfaces/todo'
-import Todo from './components/Todo';
 import TodoList from './components/TodoList';
 
 function App() {
   const [items, setItems] = useState<ITodo[]>([]);
+  const [todoTitle, setTodoTitle] = useState<string>('');
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
   useEffect(() => {
     const load = async () => {
       const data = await window.ipcRenderer.store.get('items') as ITodo[];
@@ -14,15 +17,33 @@ function App() {
     load();
   }, []);
 
-  // function addTodo(todo: ITodo) {
-  //   const newItems = [...items, todo];
-  //   setItems(newItems);
-  //   window.ipcRenderer.store.set('items', newItems);
-  // }
+  function addTodo(todo: ITodo) {
+    const newItems = [...items, todo];
+    setItems(newItems);
+    window.ipcRenderer.store.set('items', newItems);
+  }
+
+  function handleSubmit() {
+    if (todoTitle) {
+      addTodo({ title: todoTitle, done: false, created: new Date() });
+      setTodoTitle('')
+      inputRef.current?.blur();
+    }
+  }
+
+  function dropFocus() {
+    inputRef.current?.blur();
+  }
 
   return (
     <>
-      <TodoList todos={items}></TodoList>
+      <div className="bodyList">
+        <input className="todoInput" value={todoTitle}
+          onChange={(e) => setTodoTitle(e.target.value)}
+          onKeyDown={(e) => { if (e.key === 'Enter') { handleSubmit() } else if (e.key === 'Escape') dropFocus() }}
+          ref={inputRef} />
+        <TodoList todos={items}></TodoList>
+      </div>
     </>
   )
 }
